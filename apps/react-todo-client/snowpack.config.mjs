31f309b19239084d2260b2787b21652557902b04
Wayne Van Son <waynevanson@gmail.com>
2022-01-13 16:02:08 +1100

    chore(react-todo-client): alias react with preact

--- apps/react-todo-client/snowpack.config.mjs
+++ apps/react-todo-client/snowpack.config.mjs
@@ -3,4 +3,10 @@ export default {
     src: "/dist",
     public: "/",
   },
+  alias: {
+    react: "preact/compat",
+    "react-dom/test-utils": "preact/test-utils",
+    "react-dom": "preact/compat",
+    "react/jsx-runtime": "preact/jsx-runtime",
+  },
 };
